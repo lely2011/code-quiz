4dca0eaf31e02e2d772da3a71611dfaedc26c94e
LelyH <lely2011@gmail.com>
2022-09-12 21:59:26 -0700

    added html, css, and partial JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var Question = document.querySelector(".Question")
+var timerElement = document.querySelector(".timer-count");
+var StartButton = document.querySelector(".start-button");
+
+Var Question = Question1()
