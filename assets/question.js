02c1473663e96b7525363ee9e096ba2ff731a5f6
LelyH <lely2011@gmail.com>
2023-02-11 16:30:41 -0800

    added quiz function js

---
+++ assets/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        number: 1,
+        question: "What type of doctor studies the heart?",
+        answer: "Cardiologist",
+        options: [
+            "Endocrinologist",
+            "Dermatologist",
+            "Cardiologist",
+            "Neurologist",
+        ]
+    },
+    {
+        number: 2,
+        question: "What is a common cholesterol medication used?",
+        answer: "Atorvastatin",
+        options: [
+            "Metoprolol",
+            "Atorvastatin",
+            "Cetrizine",
+            "Levothyroxine",
+        ]
+    },
+    {
+        number: 3,
+        question: "What is the average heart beats per minute?",
+        answer: "60-100",
+        options: [
+            "50-90",
+            "80-110",
+            "70-120",
+            "60-100",
+        ]
+    },
+    {
+        number: 4,
+        question: "Which heart chamber pushes blood to the organs of the body?",
+        answer: "left ventricle",
+        options: [
+            "left atrium",
+            "left ventricle",
+            "right atrium",
+            "right ventricle",
+        ]
+    },
+    {
+        number: 5,
+        question: "How many chambers are in a heart?",
+        answer: "4",
+        options: [
+            "2",
+            "6",
+            "3",
+            "4",
+        ]
+    },
+];
